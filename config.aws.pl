cf3d2e9a467d17e35c2cadbf59cac1ff803fea1b
Kenneth Durbrow <kmdurbrow@me.com>
2022-08-24 16:29:26 -0400

    minor fixes

--- config.aws.pl
+++ config.aws.pl
@@ -1,14 +1,16 @@
-sub SDL_LOCATION { 's3' }
-sub NATIVE_TOOL_NAME { 'aws' }
-sub NATIVE_TOOL_CMD { 'aws s3 cp' }
-sub NATIVE_TOOL_URL {
-    $_ =~ qr{^https://([^.]+)\.s3\.([^.]+\.)?amazonaws\.com/(.+)$}i
-    ? { 'url' => "s3://$1/$3", 'region' => $2 }
-    : undef
-}
-sub NATIVE_TOOL_COPY_CMD {
-    my @y = qw[ aws s3 cp ];
-    push @y, '--source-region', $_[0]->{'region'} if $_[0]->{'region'};
-    push @y, $_[0]->{'url'}, './';
-    @y
+{
+    SDL_LOCATION => 's3',
+    NATIVE_TOOL_NAME => 'aws',
+    NATIVE_TOOL_CMD => 'aws s3 cp',
+    NATIVE_TOOL_URL => sub {
+        $_ =~ qr{^https://([^.]+)\.s3\.([^.]+\.)?amazonaws\.com/(.+)$}i
+        ? { 'url' => "s3://$1/$3", 'region' => $2 }
+        : undef
+    },
+    NATIVE_TOOL_COPY_CMD => sub {
+        my @y = qw[ aws s3 cp --no-sign-request ];
+        push @y, '--source-region', $_[0]->{'region'} if $_[0]->{'region'};
+        push @y, $_[0]->{'url'}, './';
+        @y
+    }
 }
--- config.gsutil.pl
+++ config.gsutil.pl
@@ -1,5 +1,7 @@
-sub SDL_LOCATION { 'gs' }
-sub NATIVE_TOOL_NAME { 'gsutil' }
-sub NATIVE_TOOL_CMD { 'gsutil cp' }
-sub NATIVE_TOOL_URL { $_ =~ qr{^https://storage\.googleapis\.com/(.+)$}i ? "gs://$1" : undef }
-sub NATIVE_TOOL_COPY_CMD { ('gsutil', 'cp', $_[0], './') }
+{
+    SDL_LOCATION => 'gs',
+    NATIVE_TOOL_NAME => 'gsutil',
+    NATIVE_TOOL_CMD => 'gsutil cp',
+    NATIVE_TOOL_URL => sub { $_ =~ qr{^https://storage\.googleapis\.com/(.+)$}i ? "gs://$1" : undef },
+    NATIVE_TOOL_COPY_CMD => sub { ('gsutil', 'cp', $_[0], './') }
+}
