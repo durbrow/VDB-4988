06b4fe3f3bba4a0bf2e738e000930366d86c717d
Kenneth Durbrow <kmdurbrow@me.com>
2022-08-26 12:59:04 -0400

    propagate args

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 WORKDIR=$(mktemp -d) || exit 1
-docker run -v ${WORKDIR}:/output:rw -w /output -it --rm $(docker build -q --target ${TARGET:-aws} .)
+docker run -v ${WORKDIR}:/output:rw -w /output -it --rm $(docker build -q --target ${TARGET:-aws} .) "$@"
 du -sh ${WORKDIR}
 rm -rf ${WORKDIR}
