ca24ab3024f1cbc0048831f5f6b403c2da77354c
Kenneth Durbrow <kmdurbrow@me.com>
2022-08-26 11:44:59 -0400

    Fixed EBI

--- config.aws.pl
+++ config.aws.pl
@@ -2,11 +2,6 @@
     SDL_LOCATION => 's3',
     NATIVE_TOOL_NAME => 'aws',
     NATIVE_TOOL_CMD => 'aws s3 cp',
-    NATIVE_TOOL_URL => sub {
-        $_ =~ qr{^https://([^.]+)\.s3\.([^.]+\.)?amazonaws\.com/(.+)$}i
-        ? { 'url' => "s3://$1/$3", 'region' => $2 }
-        : undef
-    },
     NATIVE_TOOL_COPY_CMD => sub {
         my @y = qw[ aws s3 cp --no-sign-request ];
         push @y, '--source-region', $_[0]->{'region'} if $_[0]->{'region'};
--- config.gsutil.pl
+++ config.gsutil.pl
@@ -2,6 +2,5 @@
     SDL_LOCATION => 'gs',
     NATIVE_TOOL_NAME => 'gsutil',
     NATIVE_TOOL_CMD => 'gsutil cp',
-    NATIVE_TOOL_URL => sub { $_ =~ qr{^https://storage\.googleapis\.com/(.+)$}i ? "gs://$1" : undef },
-    NATIVE_TOOL_COPY_CMD => sub { ('gsutil', 'cp', $_[0], './') }
+    NATIVE_TOOL_COPY_CMD => sub { ('gsutil', 'cp', $_[0]->{'url'}, './') }
 }
