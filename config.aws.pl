b2db2dcc5edc48f5aa89cc3ed0470c80714cd2a2
Kenneth Durbrow <kmdurbrow@me.com>
2022-08-26 13:32:39 -0400

    cleaned up verbosity

--- config.aws.pl
+++ config.aws.pl
@@ -5,6 +5,8 @@
     NATIVE_TOOL_COPY_CMD => sub {
         my @y = qw[ aws s3 cp --no-sign-request ];
         push @y, '--source-region', $_[0]->{'region'} if $_[0]->{'region'};
+        push @y, '--quiet' unless ($_[1] // 0);
+        push @y, '--dryrun' if $_[2];
         push @y, $_[0]->{'url'}, './';
         @y
     }
--- config.gsutil.pl
+++ config.gsutil.pl
@@ -2,5 +2,10 @@
     SDL_LOCATION => 'gs',
     NATIVE_TOOL_NAME => 'gsutil',
     NATIVE_TOOL_CMD => 'gsutil cp',
-    NATIVE_TOOL_COPY_CMD => sub { ('gsutil', 'cp', $_[0]->{'url'}, './') }
+    NATIVE_TOOL_COPY_CMD => sub {
+        my @y = ( 'gsutil' );
+        push @y, '-q' unless ($_[1] // 0);
+        push @y, 'cp', $_[0]->{'url'}, './';
+        @y
+    }
 }
